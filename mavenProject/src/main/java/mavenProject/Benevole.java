4b40018c7ecf3616a9d2c738130c330ea8f3deb1
PDLAProject <le.norgeux@gmail.com>
2023-10-13 17:40:28 +0200

    écriture des classes

---
+++ mavenProject/src/main/java/mavenProject/Benevole.java
@@ -0,0 +1,7 @@
+package mavenProject;
+
+public class Benevole {
+	
+	
+
+}
---
+++ mavenProject/src/main/java/mavenProject/User.java
@@ -0,0 +1,27 @@
+package mavenProject;
+
+public class User {
+	
+		private int idUser;
+		private String nom;
+		private String prenom;
+		private int age;
+		private String email;
+		private String telephone;
+		private String ville;
+		private String adresse;
+		
+		
+		public User(int idUser, String nom, String prenom, int age, String email, String telephone, String ville, String adresse) {
+			this.idUser = idUser;
+			this.nom = nom;
+			this.prenom = prenom;
+			this.age = age;
+			this.email = email;
+			this.telephone = telephone;
+			this.ville = ville;
+			this.adresse = adresse;
+		}
+		
+
+}
